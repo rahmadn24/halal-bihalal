8b38c3230934d2facd179fcbb024d1372e252639
rahmadn24 <r4hmadn@gmail.com>
2025-04-13 03:32:40 +0700

    update again

--- frontend/utils/db.js
+++ frontend/utils/db.js
@@ -1,4 +1,5 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: "postgresql://postgres:sprPlIgYvBbJubPOxxqQYqiODcAkjYOJ@hopper.proxy.rlwy.net:31690/railway",
